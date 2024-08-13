3dffbb8e1a2fea9e74f022c57b1d34a279c9357d
anuragverma25 <117182887+anuragverma25@users.noreply.github.com>
2024-08-13 18:01:17 +0000

    deploy nhi krna

---
+++ src/components/icons/logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const IconLogo = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <title>Logo</title>
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+        <path
+          d="M10 61 L30 20 L50 61 L45 61 L40 52 L20 52 L15 61 Z M22 48 L38 48 L30 30 M45 61 L60 20 L65 20 L50 61 Z"
+          //d="M10 61 L30 20 L50 61 L45 61 L40 52 L20 52 L15 61 Z M22 48 L38 48 L30 30 M45 61 L60 20 L65 20 L50 61 Z"
+          fill="currentColor"></path>
+      </g>
+    </g>
+  </svg>
+);
+
+export default IconLogo;
---
+++ src/components/sections/about.js
@@ -0,0 +1,188 @@
+import React, { useEffect, useRef } from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import styled from 'styled-components';
+import { srConfig } from '@config';
+import sr from '@utils/sr';
+import { usePrefersReducedMotion } from '@hooks';
+
+const StyledAboutSection = styled.section`
+  max-width: 900px;
+
+  .inner {
+    display: grid;
+    grid-template-columns: 3fr 2fr;
+    grid-gap: 50px;
+
+    @media (max-width: 768px) {
+      display: block;
+    }
+  }
+`;
+const StyledText = styled.div`
+  ul.skills-list {
+    display: grid;
+    grid-template-columns: repeat(2, minmax(140px, 200px));
+    grid-gap: 0 10px;
+    padding: 0;
+    margin: 20px 0 0 0;
+    overflow: hidden;
+    list-style: none;
+
+    li {
+      position: relative;
+      margin-bottom: 10px;
+      padding-left: 20px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: 0;
+        color: var(--green);
+        font-size: var(--fz-sm);
+        line-height: 12px;
+      }
+    }
+  }
+`;
+const StyledPic = styled.div`
+  position: relative;
+  max-width: 300px;
+
+  @media (max-width: 768px) {
+    margin: 50px auto 0;
+    width: 70%;
+  }
+
+  .wrapper {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    width: 100%;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
+
+    &:hover,
+    &:focus {
+      outline: 0;
+      transform: translate(-4px, -4px);
+
+      &:after {
+        transform: translate(8px, 8px);
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 14px;
+      left: 14px;
+      z-index: -1;
+    }
+  }
+`;
+
+const About = () => {
+  const revealContainer = useRef(null);
+  const prefersReducedMotion = usePrefersReducedMotion();
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    sr.reveal(revealContainer.current, srConfig());
+  }, []);
+
+  const skills = [
+    'Agile Business Analysis',
+    'JIRA,Confluence',
+    'Process Mapping and Optimization',
+    'Stakeholder Management',
+    'Requirement Analysis and Documentation',
+    'SQL',
+  ];
+
+  return (
+    <StyledAboutSection id="about" ref={revealContainer}>
+      <h2 className="numbered-heading">About Me</h2>
+
+      <div className="inner">
+        <StyledText>
+          <div>
+            <p>
+              Hello! My name is Anurag Verma, and I am passionate about transforming business needs
+              into technical solutions. My journey began as a software engineer at a world leading{' '}
+              <a href="https://www.cgi.com/en">IT and business consulting company</a> , where I
+              honed my coding skills and developed a solid technical foundation. This experience
+              ignited my interest in business analysis, leading me to pivot my career towards
+              bridging the gap between business strategy &amp; technology.
+            </p>
+
+            <p>
+              Fast-forward to today, and I’ve had the privilege of working as a Business Analyst at
+              leading companies like{' '}
+              <a href="https://www.lowes.com/">FORTUNE® 50 home improvement </a> company and a
+              leading{' '}
+              <a href="https://www.aptean.com/en-US">provider of industry-specific software.</a> My
+              focus is on leveraging agile methodologies to deliver impactful tech solutions,
+              optimize business processes, and drive positive outcomes.
+            </p>
+
+            <p>Here are a few skills and tools I’ve been working with recently:</p>
+          </div>
+
+          <ul className="skills-list">
+            {skills && skills.map((skill, i) => <li key={i}>{skill}</li>)}
+          </ul>
+        </StyledText>
+
+        <StyledPic>
+          <div className="wrapper">
+            <StaticImage
+              className="img"
+              src="../../images/me.jpg"
+              width={500}
+              quality={95}
+              formats={['AUTO', 'WEBP', 'AVIF']}
+              alt="Headshot"
+            />
+          </div>
+        </StyledPic>
+      </div>
+    </StyledAboutSection>
+  );
+};
+
+export default About;
